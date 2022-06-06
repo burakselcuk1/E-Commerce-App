4abdc6a97f82c97d1f593b3788fb83daf363fb70
burak <design.burakk@gmail.com>
2022-06-06 15:04:23 +0300

    HomePageFragment.kt created

--- app/src/main/java/com/example/e_commerce/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/MainActivity.kt
@@ -7,16 +7,16 @@ import android.os.Looper
 import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val anim = findViewById<LottieAnimationView>(R.id.animationView)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            anim.playAnimation()
-        },2000)
+
 
     }
 }
