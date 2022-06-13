30e4dba7965034043eea4dccd6629edb280e312b
burak <design.burakk@gmail.com>
2022-06-13 15:41:38 +0300

    some changes

--- app/src/main/java/com/example/e_commerce/ui/login/signup/SignupFragment.kt
+++ app/src/main/java/com/example/e_commerce/ui/login/signup/SignupFragment.kt
@@ -1,13 +1,16 @@
 package com.example.e_commerce.ui.login.signup
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.e_commerce.MainActivity
 import com.example.e_commerce.R
 import com.example.e_commerce.databinding.FragmentSignupBinding
 import com.example.e_commerce.ui.base.BaseFragment
+import com.example.e_commerce.ui.login.LoginActivity
 
 
 class SignupFragment : BaseFragment<FragmentSignupBinding, SignUpViewModel>(
@@ -15,8 +18,11 @@ class SignupFragment : BaseFragment<FragmentSignupBinding, SignUpViewModel>(
     viewModelClass = SignUpViewModel::class.java
 ) {
     override fun onInitDataBinding() {
-
+        with(binding){
+            appCompatButton.setOnClickListener {
+                val intent = Intent(this@SignupFragment.requireContext(), MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
-
-
 }
