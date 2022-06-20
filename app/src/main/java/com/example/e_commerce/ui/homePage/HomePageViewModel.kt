6d12a3462b4166ff196f5d143e201cd0642a4a1c
burak <design.burakk@gmail.com>
2022-06-20 15:32:05 +0300

    coroutine added

--- app/src/main/java/com/example/e_commerce/ui/homePage/HomePageViewModel.kt
+++ app/src/main/java/com/example/e_commerce/ui/homePage/HomePageViewModel.kt
@@ -1,15 +1,23 @@
 package com.example.e_commerce.ui.homePage
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.e_commerce.data.Category
 import com.example.e_commerce.data.CategoryItems
 import com.example.e_commerce.viewModels.BaseViewModel
+import kotlinx.coroutines.launch
 
 class HomePageViewModel: BaseViewModel() {
 
-    val cateforyValues = MutableLiveData<ArrayList<Category>>()
+    private val _categryValues : MutableLiveData<ArrayList<Category>> = MutableLiveData()
+
+    val cateforyValues : MutableLiveData<ArrayList<Category>> = _categryValues
     val c = CategoryItems()
+
     init {
-        cateforyValues.postValue(c.defineDatas())
+        viewModelScope.launch {
+            _categryValues.postValue(c.defineDatas())
+        }
     }
 }
