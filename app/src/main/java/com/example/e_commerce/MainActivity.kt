3f376388b6702a705f2ccda716f6904b34874157
burak <design.burakk@gmail.com>
2022-06-28 16:52:37 +0300

    toolbar fixed

--- app/src/main/java/com/example/e_commerce/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/MainActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.airbnb.lottie.LottieAnimationView
 import com.example.e_commerce.databinding.ActivityMainBinding
 import com.example.e_commerce.ui.base.BaseActivity
@@ -18,9 +21,19 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(
     layoutId = R.layout.activity_main,
     viewModelClass = MainViewModel::class.java
 ) {
+    private lateinit var navController: NavController
 
     override fun onInitDataBinding() {
-        //Bottom Nav Menu
+
+        //Init BottomNavMenu
+        navController = findNavController(R.id.fragmentContainerView)
+        setupWithNavController(binding.bottomNavigationView4,navController)
+
+        //Init Toolbar
+        setSupportActionBar(findViewById(R.id.toolbar2))
+        val navController = findNavController(R.id.fragmentContainerView)
+        val config = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar2).setupWithNavController(navController, config)
 
 
     }
