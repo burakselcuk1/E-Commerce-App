cc21e67de197fae85d4f750e0b71a02ecb3be65e
burak <design.burakk@gmail.com>
2022-06-29 16:29:46 +0300

    some changes

--- app/src/main/java/com/example/e_commerce/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -25,23 +26,45 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(
 
     override fun onInitDataBinding() {
 
-        //Init BottomNavMenu
+
+
+
+
+        InıtBottomNavMenu()
+        floatButtonClick()
+        bottomNavProperties()
+        floatButtonProperties()
+        InıtToolbdar()
+
+    }
+
+    //Init BottomNavMenu
+    private fun InıtBottomNavMenu() {
         navController = findNavController(R.id.fragmentContainerView)
         setupWithNavController(binding.bottomNavigationView4,navController)
+    }
 
-        //Init Toolbar
+    //Init Toolbar
+    private fun InıtToolbdar() {
         setSupportActionBar(findViewById(R.id.toolbar2))
         val navController = findNavController(R.id.fragmentContainerView)
         val config = AppBarConfiguration(navController.graph)
         findViewById<Toolbar>(R.id.toolbar2).setupWithNavController(navController, config)
-
-
-        binding.bottomNavigationView4.background = null
-        binding.floatbutton.background = null
-
-
     }
 
+    private fun floatButtonProperties() {
+        binding.floatbutton.background = null
+    }
+
+    private fun bottomNavProperties() {
+        binding.bottomNavigationView4.background = null
+    }
+
+    private fun floatButtonClick() {
+        binding.floatbutton.setOnClickListener {
+            Toast.makeText(it!!.context, "Float Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
 
 
 }
